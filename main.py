23a47e512c5bfc78e35f7d2f242d95e765606825
chenxb <583123293@qq.com>
2021-04-02 09:23:54 +0800

    fix bug

--- main.py
+++ main.py
@@ -22,11 +22,11 @@ from PyQt5.QtWidgets import QMainWindow,QFileDialog,QMessageBox,QApplication
 from mainwindow import *
 
 
-class Thread(QThread):
+""" class Thread(QThread):
     def __init__(self):
         super(Thread, self).__init__()
     def run(self):
-        pass
+        pass """
     
 class DisplayWindow(QMainWindow,Ui_MainWindow):
 
@@ -37,28 +37,33 @@ class DisplayWindow(QMainWindow,Ui_MainWindow):
     def setUi(self):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        #self.InstrumentSetup()
+        self.InstrumentSetup()
 
         self.demodem_1 = self.ui.comboBox.currentText()
         self.symbol_rate_1 = self.ui.lineEdit.text()
         self.center_freq_1 = self.ui.lineEdit_2.text()
         self.filter_val_1 = self.ui.lineEdit_3.text()
 
+        self.times = 0
+        self.ber_count = 0
+        self.error_count_total = 0
+        self.ber = 0 
+
         # plot timer
         # self.IQ_data = []
         # threading.Timer(1,self.PlotMap).start()
 
-        self.ui.graphicsView.setRange(xRange=(-3,3), yRange=(-3,3),disableAutoRange=True)
+        self.ui.graphicsView.setRange(xRange=(-1,1), yRange=(-1,1),disableAutoRange=True)
         self.ui.graphicsView.setMouseEnabled(x=False, y=False)
-        self.ui.graphicsView_2.setRange(xRange=(-3,3), yRange=(-3,3),disableAutoRange=True)
+        self.ui.graphicsView_2.setRange(xRange=(-1,1), yRange=(-1,1),disableAutoRange=True)
         self.ui.graphicsView_2.setMouseEnabled(x=False, y=False)
 
         # channel 1
-        self.ui.pushButton.clicked.connect(lambda:self.startDemod(self.ui.comboBox_3.currentText(),self.ui.comboBox.currentText(),self.ui.lineEdit.text(),self.ui.lineEdit_2.text(),self.ui.lineEdit_3.text(),self.appMeasItem_1,1,self.ui.graphicsView,self.ui.label_16)
+        self.ui.pushButton.clicked.connect(lambda:self.startDemod(self.ui.comboBox_3.currentText(),self.ui.comboBox.currentText(),self.ui.lineEdit.text(),self.ui.lineEdit_2.text(),self.ui.lineEdit_3.text(),self.appMeasItem_1,1,self.ui.graphicsView,self.ui.label_16))
         self.ui.pushButton_2.clicked.connect(lambda:self.stopDemod(self.timer1,self.ui.label_16,self.ui.graphicsView))
         
         # channel 2
-        self.ui.pushButton_3.clicked.connect(lambda:self.StartDemod(self.ui.comboBox_4.currentText(),self.ui.comboBox_2.currentText(),self.ui.lineEdit_4.text(),self.ui.lineEdit_5.text(),self.ui.lineEdit_6.text(),self.appMeasItem_2,2,self.ui.graphicsView_2,self.ui.label_17))
+        self.ui.pushButton_3.clicked.connect(lambda:self.startDemod(self.ui.comboBox_4.currentText(),self.ui.comboBox_2.currentText(),self.ui.lineEdit_4.text(),self.ui.lineEdit_5.text(),self.ui.lineEdit_6.text(),self.appMeasItem_2,2,self.ui.graphicsView_2,self.ui.label_17))
         self.ui.pushButton_4.clicked.connect(lambda:self.stopDemod(self.timer1,self.ui.label_16,self.ui.graphicsView))
 
         #self.ui.pushButton_5.clicked.connect(lambda: self.showDialog(self.ui.label_18))
@@ -89,7 +94,7 @@ class DisplayWindow(QMainWindow,Ui_MainWindow):
         self.app.Title = 'Demod Measurement Test'
         # Create four measurement
         self.appMeasItem_1 = self.app.Measurements.SelectedItem
-        self.appMeasItem_2 = self.app.Measurements.SelectedItem
+        #self.appMeasItem_2 = self.app.Measurements.SelectedItem
         # self.appMeasItem_2 = self.app.Measurements.SelectedItem # Need to be overWrite 
 
     # def demod_thread(self):
@@ -119,7 +124,9 @@ class DisplayWindow(QMainWindow,Ui_MainWindow):
         measHandle.ResultLength = 4000
         measHandle.FilterAlphaBT = filter_val
         filterType = getattr(MeasurementFilter,'None')
-        measHandle.MeasurementFilter = filterType 
+        measHandle.MeasurementFilter = filterType
+        measHandle.RecallStateDefinitionsFile(r'C:\Users\Administrator\Desktop\VSABitErrorRate\QAM32.csd')
+ 
 
         # 设置中心频率和显示频谱宽度
         appFreq = appMeas.Frequency
@@ -156,16 +163,18 @@ class DisplayWindow(QMainWindow,Ui_MainWindow):
 
 
     def initChannelTimer(self,appTrace_IQ,appTrace_symbol,Data_origin,graphViewer,channel_num,demodem):
-        self.serial_plot_timer = QtCore.QTimer()
-        self.serial_plot_timer.timeout.connect(lambda: self.DisplayAll(appTrace_IQ,appTrace_symbol,Data_origin,graphViewer,channel_num,demodem))
-        self.serial_plot_timer.start(2000) 
+        exec('self.plot_timer{} = QtCore.QTimer()'.format(channel_num))
+        timer = eval('self.plot_timer{}'.format(channel_num))
+        timer.timeout.connect(lambda: self.DisplayAll(appTrace_IQ,appTrace_symbol,Data_origin,demodem,channel_num,graphViewer))
+        timer.start(100)
+        # self.plot_timer = QtCore.QTimer()
+        # self.plot_timer.timeout.connect(lambda: self.DisplayAll(self,appTrace_IQ,appTrace_symbol,Data_origin,demodem,channel_num,graphViewer))
+        # self.plot_timer.start(100)
 
-    
     def DisplayAll(self,appTrace_IQ,appTrace_symbol,Data_origin,demodem,channel_num,graphViewer):
         self.PlotMap(appTrace_IQ,graphViewer,channel_num)
         self.BerCal(Data_origin,appTrace_symbol,channel_num,demodem)
-
-
+     
     def PlotMap(self,appTrace_IQ,graphViewer,channel_num):
         file_path = r'C:\Users\Administrator\Desktop\VSABitErrorRate\demod_Qam_IQ_Data_channel'+str(channel_num)+r'.csv'
         appTrace_IQ.SaveFile(file_path,'CSV', False)
@@ -182,63 +191,58 @@ class DisplayWindow(QMainWindow,Ui_MainWindow):
 
 
     def BerCal(self,Data_origin,appTrace_symbol,channel_num,demodem):
+        demod_N = demodem[3:] # value is 16 32 64 or 128
+        Demod_N=int(demod_N)
+        M=int(math.log(Demod_N,2))   
+        self.ber_count += 1
+        if self.ber_count==200:
+            self.times = 0
+            self.ber_count = 0
+            self.error_count_total = 0
+            self.ber = 0 
+
         file_path = r'C:\Users\Administrator\Desktop\VSABitErrorRate\demod_Qam_Symbol_Data_channel'+str(channel_num)+r'.csv'
         appTrace_symbol.SaveFile(file_path,'CSV', False)
         Data_output = pd.read_csv(file_path)  
-        demod_N = demodem[3:] # value is 16 32 64 or 128
         symbol_list = Data_output.T.values.tolist()  # demod symbol 
-        
         origin_list = Data_origin.T.values.tolist()
-        bit_Data_origin=[]
-        bit_symbol_list=[]
 
-        Demod_N=int(demod_N)
-        M=int(math.log(Demod_N,2))
+        bit_Data_origin = []
+        bit_symbol_list = []
+
+        for i in origin_list[0]:
+            origin_bin = bin(i)
+            origin_bin_list = list(origin_bin[2:])
+            zeros_seq = [0 for index in range(M-len(origin_bin_list))] # 生成长度为M-len(b)的零列表
+            origin_bit = np.hstack((np.array(zeros_seq),np.array(origin_bin_list)))
+            bit_Data_origin.append(list(origin_bit))
         
-        for i in range(0,len(origin_list)-1):
-            b1=bin(origin_list[i])
-            b2=b1[2:]
-            dim_1 = [0 for index in range(M-len(b2))] # 生成长度为M-len(b)的零列表
-            z1=dim_1+b2
-            bit_Data_origin=bit_Data_origin+z1
-            
-        for j in range(0,len(symbol_list)-1):
-            b3=bin(symbol_list[i])
-            b4=b3[2:]
-            dim_2 = [0 for index in range(M-len(b4))] 
-            z2=dim_2+b4
-            bit_symbol_list= bit_symbol_list+z2
-           
-        t = 0
-        Times = 500
-        findedTimes = 0
-        bitErrorRate_total = 0
-        frameHeader=bit_Data_origin[1:80]
-        ber = 0 
-        while t<Times:
-            frameHeader_rate=0
-            z=0
-            for n in range(0,79):
-                if bit_symbol_list[n]==0:
-                    z+=1
-              
-            frameHeader_rate=z/80###
-            bitErrorRate = 0
-            for g in range(0,len(bit_symbol_list)-81):
-                if frameHeader_rate>=0.5:
-                    data_after=bit_symbol_list[80,len(bit_symbol_list)]
-                    findedTimes = findedTimes + 1
-                    k=0
-                    for l in range(0,len(bit_symbol_list)-81):
-                        if data_after[l]!=bit_Data_origin[80+l]:
-                            k+=1
-                    bitErrorRate=k/len(data_after)
-            bitErrorRate_total = bitErrorRate_total + bitErrorRate
-            t = t + 1  
-        
-        Ber = bitErrorRate_total/findedTimes 
-        ber = "%.2f%%" % (Ber * 100) #百分数保留两位小数
-        return ber
+        #print('111111111111111111111111')
+        for j in symbol_list[0]:
+            symbol_bin = bin(j)
+            symbol_bin_list = list(symbol_bin[2:])
+            zeros_seq = [0 for index in range(M-len(symbol_bin_list))] # 生成长度为M-len(b)的零列表
+            symbol_bit = np.hstack((np.array(zeros_seq),np.array(symbol_bin_list)))
+            bit_symbol_list.append(list(symbol_bit))
+
+        #print('222222222222222222222222')
+        frameHeader = bit_Data_origin[0:80]
+        frameHeader_array = np.array(frameHeader,dtype='int')
+        count_array = np.array(bit_symbol_list[0:80],dtype='int')-frameHeader_array
+        count = 80 - np.sum(count_array==0)   
+        match_rate = count/80 
+
+        if match_rate>=0.5:
+            self.times += 1
+            data_array = np.array(bit_symbol_list[80:],dtype='int')
+            origin_data_array = np.array(bit_Data_origin[80:],dtype='int')
+            error = data_array-origin_data_array
+            error_count = len(bit_symbol_list[80:]) - np.sum(error==0)
+            self.error_count_total = self.error_count_total+error_count
+            self.ber =  self.error_count_total/(self.times*len(bit_symbol_list[80:]))
+
+        print('ber is',self.ber)
+        self.ui.label_21.setText(self.ber)
     
     def DataRateCal(self): 
         return None
